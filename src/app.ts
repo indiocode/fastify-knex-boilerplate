5de1402b42e73611f0a8ee25d0e0667a50d67fb7
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-19 20:37:50 -0300

    :sparkles: configure middlewares and cookies

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cookie from '@fastify/cookie';
+import fastify from 'fastify';
+
+import { usersRoutes } from './routes/users';
+
+export const app = fastify();
+
+// app.addHook('preHandler', async (request, reply) => {
+// 	console.log(`[${request.method}] ${request.url}`);
+// });
+
+app.register(cookie);
+
+app.register(usersRoutes, {
+	prefix: 'transactions',
+});
