c51d639605948abab1daf29aec1bd3191dbc3db0
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-19 20:30:16 -0300

    :sparkles: configure env

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+process.env.NODE_ENV === 'test' ? config({ path: '.env.test' }) : config();
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+	DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+	DATABASE_URL: z.string(),
+	PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+	console.error('Invalid environment variables!', _env.error.format());
+
+	throw new Error('Invalid environment variables.');
+}
+
+export const env = _env.data;
